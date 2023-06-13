f66c07e18ef9846f93c7c252e1c5eff9eb182304
Jongmin Kim <whdalsrnt@megazone.com>
2023-06-14 00:35:30 +0900

    feat: add region metadata

--- src/cloudforet/plugin/model/prowler/collector.py
+++ src/cloudforet/plugin/model/prowler/collector.py
@@ -39,6 +39,42 @@ COMPLIANCE_TYPES = {
     }
 }
 
+REGIONS = {
+    'aws': [
+        'af-south-1',
+        'ap-east-1',
+        'ap-northeast-1',
+        'ap-northeast-2',
+        'ap-northeast-3',
+        'ap-south-1',
+        'ap-south-2',
+        'ap-southeast-1',
+        'ap-southeast-2',
+        'ap-southeast-3',
+        'ap-southeast-4',
+        'ca-central-1',
+        'cn-north-1',
+        'cn-northwest-1',
+        'eu-central-1',
+        'eu-central-2',
+        'eu-north-1',
+        'eu-south-1',
+        'eu-south-2',
+        'eu-west-1',
+        'eu-west-2',
+        'eu-west-3',
+        'me-central-1',
+        'me-south-1',
+        'sa-east-1',
+        'us-east-1',
+        'us-east-2',
+        'us-gov-east-1',
+        'us-gov-west-1',
+        'us-west-1',
+        'us-west-2'
+    ]
+}
+
 SERVICES = {
     'aws': {
         'AccessAnalyzer': 'accessanalyzer',
@@ -126,7 +162,7 @@ class AWSPluginInfo(PluginInfo):
         ],
         'options_schema': {
             'required': ['provider', 'compliance_type'],
-            'order': ['provider', 'compliance_type'],
+            'order': ['provider', 'compliance_type', 'regions'],
             'type': 'object',
             'properties': {
                 'provider': {
@@ -139,6 +175,13 @@ class AWSPluginInfo(PluginInfo):
                     'title': 'Compliance Type',
                     'type': 'string',
                     'enum': list(COMPLIANCE_TYPES['aws'].keys()),
+                },
+                'regions': {
+                    'title': 'Region',
+                    'type': 'array',
+                    'items': {
+                        'enum': REGIONS['aws']
+                    }
                 },
                 # 'services': {
                 #     'title': 'Service',
