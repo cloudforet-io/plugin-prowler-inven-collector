95b735a4ab5afed3c90ee4d2ddc3958dd8f11555
ImMin5 <mino@megazone.com>
2024-01-11 02:02:29 +0900

    feat: add new provider azure with Azure-Standard compliance framework

--- src/cloudforet/plugin/connector/aws_prowler_connector.py
+++ src/cloudforet/plugin/connector/aws_prowler_connector.py
@@ -10,11 +10,13 @@ from spaceone.core.connector import BaseConnector
 from cloudforet.plugin.error.custom import *
 from cloudforet.plugin.model.prowler.collector import COMPLIANCE_FRAMEWORKS
 
-__all__ = ['AWSProwlerConnector']
+__all__ = ["AWSProwlerConnector"]
 
 _LOGGER = logging.getLogger(__name__)
-_AWS_PROFILE_PATH = os.environ.get('AWS_SHARED_CREDENTIALS_FILE', os.path.expanduser('~/.aws/credentials'))
-_AWS_PROFILE_DIR = _AWS_PROFILE_PATH.rsplit('/', 1)[0]
+_AWS_PROFILE_PATH = os.environ.get(
+    "AWS_SHARED_CREDENTIALS_FILE", os.path.expanduser("~/.aws/credentials")
+)
+_AWS_PROFILE_DIR = _AWS_PROFILE_PATH.rsplit("/", 1)[0]
 
 
 class AWSProfileManager:
@@ -39,7 +41,7 @@ class AWSProfileManager:
     def credentials(self) -> dict:
         return self._credentials
 
-    def __enter__(self) -> 'AWSProfileManager':
+    def __enter__(self) -> "AWSProfileManager":
         self._add_aws_profile()
         return self
 
@@ -47,7 +49,7 @@ class AWSProfileManager:
         self._remove_aws_profile()
 
     def _add_aws_profile(self):
-        _LOGGER.debug(f'[_AWSProfileManager] add aws profile: {self._profile_name}')
+        _LOGGER.debug(f"[_AWSProfileManager] add aws profile: {self._profile_name}")
 
         aws_profile = configparser.ConfigParser()
 
@@ -61,34 +63,56 @@ class AWSProfileManager:
 
         aws_profile.add_section(self.profile_name)
 
-        if 'role_arn' in self._credentials:
+        if "role_arn" in self._credentials:
             self.source_profile_name = utils.random_string()
             aws_profile.add_section(self.source_profile_name)
-            aws_profile.set(self.source_profile_name, 'aws_access_key_id', self._credentials['aws_access_key_id'])
-            aws_profile.set(self.source_profile_name, 'aws_secret_access_key', self._credentials['aws_secret_access_key'])
+            aws_profile.set(
+                self.source_profile_name,
+                "aws_access_key_id",
+                self._credentials["aws_access_key_id"],
+            )
+            aws_profile.set(
+                self.source_profile_name,
+                "aws_secret_access_key",
+                self._credentials["aws_secret_access_key"],
+            )
 
-            aws_profile.set(self.profile_name, 'role_arn', self._credentials['role_arn'])
-            aws_profile.set(self.profile_name, 'source_profile', self.source_profile_name)
+            aws_profile.set(
+                self.profile_name, "role_arn", self._credentials["role_arn"]
+            )
+            aws_profile.set(
+                self.profile_name, "source_profile", self.source_profile_name
+            )
 
-            if 'external_id' in self._credentials:
-                aws_profile.set(self.profile_name, 'external_id', self._credentials['external_id'])
+            if "external_id" in self._credentials:
+                aws_profile.set(
+                    self.profile_name, "external_id", self._credentials["external_id"]
+                )
 
         else:
-            aws_profile.set(self.profile_name, 'aws_access_key_id', self._credentials['aws_access_key_id'])
-            aws_profile.set(self.profile_name, 'aws_secret_access_key', self._credentials['aws_secret_access_key'])
+            aws_profile.set(
+                self.profile_name,
+                "aws_access_key_id",
+                self._credentials["aws_access_key_id"],
+            )
+            aws_profile.set(
+                self.profile_name,
+                "aws_secret_access_key",
+                self._credentials["aws_secret_access_key"],
+            )
 
-        with open(_AWS_PROFILE_PATH, 'w') as f:
+        with open(_AWS_PROFILE_PATH, "w") as f:
             aws_profile.write(f)
 
     @staticmethod
     def _create_aws_profile_file(aws_profile: configparser.ConfigParser):
         os.makedirs(_AWS_PROFILE_DIR, exist_ok=True)
-        aws_profile['default'] = {}
-        with open(_AWS_PROFILE_PATH, 'w') as f:
+        aws_profile["default"] = {}
+        with open(_AWS_PROFILE_PATH, "w") as f:
             aws_profile.write(f)
 
     def _remove_aws_profile(self, aws_profile: configparser.ConfigParser = None):
-        _LOGGER.debug(f'[_AWSProfileManager] remove aws profile: {self._profile_name}')
+        _LOGGER.debug(f"[_AWSProfileManager] remove aws profile: {self._profile_name}")
 
         if aws_profile is None:
             aws_profile = configparser.ConfigParser()
@@ -97,14 +121,18 @@ class AWSProfileManager:
         if self.profile_name in aws_profile.sections():
             aws_profile.remove_section(self.profile_name)
 
-        if self.source_profile_name and self.source_profile_name in aws_profile.sections():
+        if (
+            self.source_profile_name
+            and self.source_profile_name in aws_profile.sections()
+        ):
             aws_profile.remove_section(self.source_profile_name)
 
-        with open(_AWS_PROFILE_PATH, 'w') as f:
+        with open(_AWS_PROFILE_PATH, "w") as f:
             aws_profile.write(f)
 
 
 class AWSProwlerConnector(BaseConnector):
+    provider = "aws"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -116,48 +144,57 @@ class AWSProwlerConnector(BaseConnector):
         with tempfile.TemporaryDirectory():
             with AWSProfileManager(secret_data) as aws_profile:
                 cmd = self._command_prefix(aws_profile.profile_name)
-                cmd += ['-l']
-                _LOGGER.debug(f'[verify_client] command: {cmd}')
-                response = subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
+                cmd += ["-l"]
+                _LOGGER.debug(f"[verify_client] command: {cmd}")
+                response = subprocess.run(
+                    cmd, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE
+                )
                 if response.returncode != 0:
-                    raise ERROR_PROWLER_EXECUTION_FAILED(reason=response.stderr.decode('utf-8'))
+                    raise ERROR_PROWLER_EXECUTION_FAILED(
+                        reason=response.stderr.decode("utf-8")
+                    )
 
     def check(self, options: dict, secret_data: dict, schema: str):
         self._check_secret_data(secret_data)
-        regions = options.get('regions', [])
+        regions = options.get("regions", [])
 
-        compliance_framework = COMPLIANCE_FRAMEWORKS['aws'].get(options['compliance_framework'])
+        compliance_framework = COMPLIANCE_FRAMEWORKS["aws"].get(
+            options["compliance_framework"]
+        )
 
         with tempfile.TemporaryDirectory() as temp_dir:
             with AWSProfileManager(secret_data) as aws_profile:
                 cmd = self._command_prefix(aws_profile.profile_name)
 
-                cmd += ['-M', 'json', '-o', temp_dir, '-F', 'output', '-z']
-                cmd += ['--compliance', compliance_framework]
+                cmd += ["-M", "json", "-o", temp_dir, "-F", "output", "-z"]
+                cmd += ["--compliance", compliance_framework]
 
                 if regions:
-                    region_filter = ['-f'] + regions
+                    region_filter = ["-f"] + regions
                     cmd += region_filter
 
-                _LOGGER.debug(f'[check] command: {cmd}')
+                _LOGGER.debug(f"[check] command: {cmd}")
 
-                response = subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
+                response = subprocess.run(
+                    cmd, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE
+                )
                 if response.returncode != 0:
-                    raise ERROR_PROWLER_EXECUTION_FAILED(reason=response.stderr.decode('utf-8'))
+                    raise ERROR_PROWLER_EXECUTION_FAILED(
+                        reason=response.stderr.decode("utf-8")
+                    )
 
-                output_json_file = os.path.join(temp_dir, 'output.json')
+                output_json_file = os.path.join(temp_dir, "output.json")
                 check_results = utils.load_json_from_file(output_json_file)
                 return check_results
 
-
     @staticmethod
     def _check_secret_data(secret_data: dict):
-        if 'aws_access_key_id' not in secret_data:
-            raise ERROR_REQUIRED_PARAMETER(key='secret_data.aws_access_key_id')
+        if "aws_access_key_id" not in secret_data:
+            raise ERROR_REQUIRED_PARAMETER(key="secret_data.aws_access_key_id")
 
-        if 'aws_secret_access_key' not in secret_data:
-            raise ERROR_REQUIRED_PARAMETER(key='secret_data.aws_secret_access_key')
+        if "aws_secret_access_key" not in secret_data:
+            raise ERROR_REQUIRED_PARAMETER(key="secret_data.aws_secret_access_key")
 
     @staticmethod
     def _command_prefix(aws_profile_name: str) -> List[str]:
-        return ['python3', '-m', 'prowler', 'aws', '-p', aws_profile_name, '-b']
+        return ["python3", "-m", "prowler", "aws", "-p", aws_profile_name, "-b"]
--- src/cloudforet/plugin/model/prowler/collector.py
+++ src/cloudforet/plugin/model/prowler/collector.py
@@ -1,188 +1,189 @@
-from cloudforet.plugin.model.plugin_info_model import PluginInfo, PluginMetadata, ResourceType, ScheduleType, Feature
+from cloudforet.plugin.model.plugin_info_model import (
+    PluginInfo,
+    PluginMetadata,
+    ResourceType,
+    ScheduleType,
+    Feature,
+)
 
 SEVERITIES = {
-    'Critical': 'critical',
-    'High': 'high',
-    'Medium': 'medium',
-    'Low': 'low',
-    'Informational': 'informational',
+    "Critical": "critical",
+    "High": "high",
+    "Medium": "medium",
+    "Low": "low",
+    "Informational": "informational",
 }
 
 COMPLIANCE_FRAMEWORKS = {
-    'aws': {
-        'CIS-1.4': 'cis_1.4_aws',
-        'CIS-1.5': 'cis_1.5_aws',
-        'CISA': 'cisa_aws',
-        'SOC2': 'soc2_aws',
-        'GDPR': 'gdpr_aws',
-        'HIPAA': 'hipaa_aws',
-        'AWS-Foundational-Security-Best-Practices': 'aws_foundational_security_best_practices_aws',
-        'GxP-21-CFR-Part-11': 'gxp_21_cfr_part_11_aws',
-        'GxP-EU-Annex-11': 'gxp_eu_annex_11_aws',
-        'NIST-800-171-Revision-2': 'nist_800_171_revision_2_aws',
-        'NIST-800-53-Revision-4': 'nist_800_53_revision_4_aws',
-        'NIST-800-53-Revision-5': 'nist_800_53_revision_5_aws',
-        'ENS-RD2022': 'ens_rd2022_aws',
-        'NIST-CSF-1.1': 'nist_csf_1.1_aws',
-        'AWS-Audit-Manager-Control-Tower-Guardrails': 'aws_audit_manager_control_tower_guardrails_aws',
-        'RBI-Cyber-Security-Framework': 'rbi_cyber_security_framework_aws',
-        'FFIEC': 'ffiec_aws',
-        'PCI-3.2.1': 'pci_3.2.1_aws',
-        'FedRamp-Moderate-Revision-4': 'fedramp_moderate_revision_4_aws',
-        'FedRAMP-Low-Revision-4': 'fedramp_low_revision_4_aws',
+    "aws": {
+        "CIS-1.4": "cis_1.4_aws",
+        "CIS-1.5": "cis_1.5_aws",
+        "CISA": "cisa_aws",
+        "SOC2": "soc2_aws",
+        "GDPR": "gdpr_aws",
+        "HIPAA": "hipaa_aws",
+        "AWS-Foundational-Security-Best-Practices": "aws_foundational_security_best_practices_aws",
+        "GxP-21-CFR-Part-11": "gxp_21_cfr_part_11_aws",
+        "GxP-EU-Annex-11": "gxp_eu_annex_11_aws",
+        "NIST-800-171-Revision-2": "nist_800_171_revision_2_aws",
+        "NIST-800-53-Revision-4": "nist_800_53_revision_4_aws",
+        "NIST-800-53-Revision-5": "nist_800_53_revision_5_aws",
+        "ENS-RD2022": "ens_rd2022_aws",
+        "NIST-CSF-1.1": "nist_csf_1.1_aws",
+        "AWS-Audit-Manager-Control-Tower-Guardrails": "aws_audit_manager_control_tower_guardrails_aws",
+        "RBI-Cyber-Security-Framework": "rbi_cyber_security_framework_aws",
+        "FFIEC": "ffiec_aws",
+        "PCI-3.2.1": "pci_3.2.1_aws",
+        "FedRamp-Moderate-Revision-4": "fedramp_moderate_revision_4_aws",
+        "FedRAMP-Low-Revision-4": "fedramp_low_revision_4_aws",
     },
-    'google_cloud': {
-        'Google-Cloud-Standard': '',
+    "google_cloud": {
+        "Google-Cloud-Standard": "",
     },
-    'azure': {
-        'Azure-Standard': '',
-    }
+    "azure": {
+        "Azure-Standard": "",
+    },
 }
 
 REGIONS = {
-    'aws': [
-        'af-south-1',
-        'ap-east-1',
-        'ap-northeast-1',
-        'ap-northeast-2',
-        'ap-northeast-3',
-        'ap-south-1',
-        'ap-south-2',
-        'ap-southeast-1',
-        'ap-southeast-2',
-        'ap-southeast-3',
-        'ap-southeast-4',
-        'ca-central-1',
-        'cn-north-1',
-        'cn-northwest-1',
-        'eu-central-1',
-        'eu-central-2',
-        'eu-north-1',
-        'eu-south-1',
-        'eu-south-2',
-        'eu-west-1',
-        'eu-west-2',
-        'eu-west-3',
-        'me-central-1',
-        'me-south-1',
-        'sa-east-1',
-        'us-east-1',
-        'us-east-2',
-        'us-gov-east-1',
-        'us-gov-west-1',
-        'us-west-1',
-        'us-west-2'
+    "aws": [
+        "af-south-1",
+        "ap-east-1",
+        "ap-northeast-1",
+        "ap-northeast-2",
+        "ap-northeast-3",
+        "ap-south-1",
+        "ap-south-2",
+        "ap-southeast-1",
+        "ap-southeast-2",
+        "ap-southeast-3",
+        "ap-southeast-4",
+        "ca-central-1",
+        "cn-north-1",
+        "cn-northwest-1",
+        "eu-central-1",
+        "eu-central-2",
+        "eu-north-1",
+        "eu-south-1",
+        "eu-south-2",
+        "eu-west-1",
+        "eu-west-2",
+        "eu-west-3",
+        "me-central-1",
+        "me-south-1",
+        "sa-east-1",
+        "us-east-1",
+        "us-east-2",
+        "us-gov-east-1",
+        "us-gov-west-1",
+        "us-west-1",
+        "us-west-2",
     ]
 }
 
 SERVICES = {
-    'aws': {
-        'AccessAnalyzer': 'accessanalyzer',
-        'Account': 'account',
-        'ACM': 'acm',
-        'APIGateway': 'apigateway',
-        'APIGatewayV2': 'apigatewayv2',
-        'AppStream': 'appstream',
-        'AutoScaling': 'autoscaling',
-        'Lambda': 'awslambda',
-        'Backup': 'backup',
-        'CloudFormation': 'cloudformation',
-        'CloudFront': 'cloudfront',
-        'CloudTrail': 'cloudtrail',
-        'CloudWatch': 'cloudwatch',
-        'CodeArtifact': 'codeartifact',
-        'CodeBuild': 'codebuild',
-        'ConfigService': 'config',
-        'DirectoryService': 'directoryservice',
-        'DRS': 'drs',
-        'DynamoDB': 'dynamodb',
-        'EC2': 'ec2',
-        'ECR': 'ecr',
-        'ECS': 'ecs',
-        'EFS': 'efs',
-        'EKS': 'eks',
-        'ELB': 'elb',
-        'ELBv2': 'elbv2',
-        'EMR': 'emr',
-        'Glacier': 'glacier',
-        'Glue': 'glue',
-        'GuardDuty': 'guardduty',
-        'IAM': 'iam',
-        'Inspector2': 'inspector2',
-        'KMS': 'kms',
-        'Macie': 'macie',
-        'OpenSearchService': 'opensearch',
-        'Organizations': 'organizations',
-        'RDS': 'rds',
-        'Redshift': 'redshift',
-        'ResourceExplorer2': 'resourceexplorer2',
-        'Route53': 'route53',
-        'S3': 's3',
-        'SageMaker': 'sagemaker',
-        'SecretsManager': 'secretsmanager',
-        'SecurityHub': 'securityhub',
-        'Sheild': 'shield',
-        'SNS': 'sns',
-        'SQS': 'sqs',
-        'SSM': 'ssm',
-        'SSMIncidents': 'ssmincidents',
-        'TrustedAdvisor': 'trustedadvisor',
-        'VPC': 'vpc',
-        'WorkSpaces': 'workspaces',
+    "aws": {
+        "AccessAnalyzer": "accessanalyzer",
+        "Account": "account",
+        "ACM": "acm",
+        "APIGateway": "apigateway",
+        "APIGatewayV2": "apigatewayv2",
+        "AppStream": "appstream",
+        "AutoScaling": "autoscaling",
+        "Lambda": "awslambda",
+        "Backup": "backup",
+        "CloudFormation": "cloudformation",
+        "CloudFront": "cloudfront",
+        "CloudTrail": "cloudtrail",
+        "CloudWatch": "cloudwatch",
+        "CodeArtifact": "codeartifact",
+        "CodeBuild": "codebuild",
+        "ConfigService": "config",
+        "DirectoryService": "directoryservice",
+        "DRS": "drs",
+        "DynamoDB": "dynamodb",
+        "EC2": "ec2",
+        "ECR": "ecr",
+        "ECS": "ecs",
+        "EFS": "efs",
+        "EKS": "eks",
+        "ELB": "elb",
+        "ELBv2": "elbv2",
+        "EMR": "emr",
+        "Glacier": "glacier",
+        "Glue": "glue",
+        "GuardDuty": "guardduty",
+        "IAM": "iam",
+        "Inspector2": "inspector2",
+        "KMS": "kms",
+        "Macie": "macie",
+        "OpenSearchService": "opensearch",
+        "Organizations": "organizations",
+        "RDS": "rds",
+        "Redshift": "redshift",
+        "ResourceExplorer2": "resourceexplorer2",
+        "Route53": "route53",
+        "S3": "s3",
+        "SageMaker": "sagemaker",
+        "SecretsManager": "secretsmanager",
+        "SecurityHub": "securityhub",
+        "Sheild": "shield",
+        "SNS": "sns",
+        "SQS": "sqs",
+        "SSM": "ssm",
+        "SSMIncidents": "ssmincidents",
+        "TrustedAdvisor": "trustedadvisor",
+        "VPC": "vpc",
+        "WorkSpaces": "workspaces",
     },
-    'google_cloud': {
-        'BigQuery': 'bigquery',
-        'CloudSQL': 'cloudsql',
-        'CloudStorage': 'cloudstorage',
-        'ComputeEngine': 'compute',
-        'IAM': 'iam',
-        'KMS': 'kms',
-        'Logging': 'logging',
+    "google_cloud": {
+        "BigQuery": "bigquery",
+        "CloudSQL": "cloudsql",
+        "CloudStorage": "cloudstorage",
+        "ComputeEngine": "compute",
+        "IAM": "iam",
+        "KMS": "kms",
+        "Logging": "logging",
     },
-    'azure': {
-        'Defender': 'defender',
-        'IAM': 'iam',
-        'Storage': 'storage',
-    }
+    "azure": {
+        "Defender": "defender",
+        "IAM": "iam",
+        "Storage": "storage",
+        "SQLServer": "sqlserver",
+    },
 }
 
 
 class AWSPluginInfo(PluginInfo):
     metadata: PluginMetadata = {
-        'supported_resource_type': [
+        "supported_resource_type": [
             ResourceType.cloud_service,
             ResourceType.cloud_service_type,
-            ResourceType.region
+            ResourceType.region,
         ],
-        'supported_schedules': [
-            ScheduleType.hours
-        ],
-        'supported_features': [
-            Feature.garbage_collection
-        ],
-        'options_schema': {
-            'required': ['provider', 'compliance_framework'],
-            'order': ['provider', 'compliance_framework', 'regions'],
-            'type': 'object',
-            'properties': {
-                'provider': {
-                    'title': 'Provider',
-                    'type': 'string',
-                    'default': 'aws',
-                    'disabled': True
+        "supported_schedules": [ScheduleType.hours],
+        "supported_features": [Feature.garbage_collection],
+        "options_schema": {
+            "required": ["provider", "compliance_framework"],
+            "order": ["provider", "compliance_framework", "regions"],
+            "type": "object",
+            "properties": {
+                "provider": {
+                    "title": "Provider",
+                    "type": "string",
+                    "default": "aws",
+                    "disabled": True,
                 },
-                'compliance_framework': {
-                    'title': 'Compliance Framework',
-                    'type': 'string',
-                    'enum': list(COMPLIANCE_FRAMEWORKS['aws'].keys()),
-                    'default': 'CIS-1.5'
+                "compliance_framework": {
+                    "title": "Compliance Framework",
+                    "type": "string",
+                    "enum": list(COMPLIANCE_FRAMEWORKS["aws"].keys()),
+                    "default": "CIS-1.5",
                 },
-                'regions': {
-                    'title': 'Region Filter',
-                    'type': 'array',
-                    'items': {
-                        'enum': REGIONS['aws']
-                    }
+                "regions": {
+                    "title": "Region Filter",
+                    "type": "array",
+                    "items": {"enum": REGIONS["aws"]},
                 },
                 # 'services': {
                 #     'title': 'Service',
@@ -198,40 +199,36 @@ class AWSPluginInfo(PluginInfo):
                 #         'enum': list(SEVERITIES.keys())
                 #     }
                 # }
-            }
-        }
+            },
+        },
     }
 
 
 class GoogleCloudPluginInfo(PluginInfo):
     metadata: PluginMetadata = {
-        'supported_resource_type': [
+        "supported_resource_type": [
             ResourceType.cloud_service,
             ResourceType.cloud_service_type,
-            ResourceType.region
+            ResourceType.region,
         ],
-        'supported_schedules': [
-            ScheduleType.hours
-        ],
-        'supported_features': [
-            Feature.garbage_collection
-        ],
-        'options_schema': {
-            'required': ['provider', 'compliance_framework'],
-            'order': ['provider', 'compliance_framework'],
-            'type': 'object',
-            'properties': {
-                'provider': {
-                    'title': 'Provider',
-                    'type': 'string',
-                    'default': 'google_cloud',
-                    'disabled': True
+        "supported_schedules": [ScheduleType.hours],
+        "supported_features": [Feature.garbage_collection],
+        "options_schema": {
+            "required": ["provider", "compliance_framework"],
+            "order": ["provider", "compliance_framework"],
+            "type": "object",
+            "properties": {
+                "provider": {
+                    "title": "Provider",
+                    "type": "string",
+                    "default": "google_cloud",
+                    "disabled": True,
                 },
-                'compliance_framework': {
-                    'title': 'Compliance Framework',
-                    'type': 'string',
-                    'enum': list(COMPLIANCE_FRAMEWORKS['google_cloud'].keys()),
-                    'default': 'Google-Cloud-Standard'
+                "compliance_framework": {
+                    "title": "Compliance Framework",
+                    "type": "string",
+                    "enum": list(COMPLIANCE_FRAMEWORKS["google_cloud"].keys()),
+                    "default": "Google-Cloud-Standard",
                 },
                 # 'services': {
                 #     'title': 'Service',
@@ -247,40 +244,36 @@ class GoogleCloudPluginInfo(PluginInfo):
                 #         'enum': list(SEVERITIES.keys())
                 #     }
                 # }
-            }
-        }
+            },
+        },
     }
 
 
 class AzurePluginInfo(PluginInfo):
     metadata: PluginMetadata = {
-        'supported_resource_type': [
+        "supported_resource_type": [
             ResourceType.cloud_service,
             ResourceType.cloud_service_type,
-            ResourceType.region
+            ResourceType.region,
         ],
-        'supported_schedules': [
-            ScheduleType.hours
-        ],
-        'supported_features': [
-            Feature.garbage_collection
-        ],
-        'options_schema': {
-            'required': ['provider', 'compliance_framework'],
-            'order': ['provider', 'compliance_framework'],
-            'type': 'object',
-            'properties': {
-                'provider': {
-                    'title': 'Provider',
-                    'type': 'string',
-                    'default': 'azure',
-                    'disabled': True
+        "supported_schedules": [ScheduleType.hours],
+        "supported_features": [Feature.garbage_collection],
+        "options_schema": {
+            "required": ["provider", "compliance_framework"],
+            "order": ["provider", "compliance_framework"],
+            "type": "object",
+            "properties": {
+                "provider": {
+                    "title": "Provider",
+                    "type": "string",
+                    "default": "azure",
+                    "disabled": True,
                 },
-                'compliance_framework': {
-                    'title': 'Compliance Framework',
-                    'type': 'string',
-                    'enum': list(COMPLIANCE_FRAMEWORKS['azure'].keys()),
-                    'default': 'Azure-Standard'
+                "compliance_framework": {
+                    "title": "Compliance Framework",
+                    "type": "string",
+                    "enum": list(COMPLIANCE_FRAMEWORKS["azure"].keys()),
+                    "default": "Azure-Standard",
                 },
                 # 'services': {
                 #     'title': 'Service',
@@ -296,6 +289,6 @@ class AzurePluginInfo(PluginInfo):
                 #         'enum': list(SEVERITIES.keys())
                 #     }
                 # }
-            }
-        }
+            },
+        },
     }
